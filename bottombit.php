49fc9ca54dd4cb50bba4768cd79a945582ee7b57
kennedyt71079 <58008618+kennedyt71079@users.noreply.github.com>
2020-09-11 10:36:08 +1200

    yo broke

--- bottombit.php
+++ bottombit.php
@@ -1,7 +1,45 @@
-        <div class="box footer">
-            CC Troy Kennedy 2020
-        </div>    <!-- / footer -->
+<div class="box side">
     
-    </div>  <!-- / wrapper  -->
+    <h3>Search | <a class ="side" href="show_all.php">Show All</a>
+    </h3>
     
-</body>        
+    <i>Type part of the title / Author name and then search</i>
+
+    <hr /> 
+    
+    <!-- Start of Title Search -->
+    
+    <form method="post" actions="title_search.php" 
+    enctype="multipart/form-data">
+        
+        <input class="search" type="text" size="40" value=""
+        required placeholder="Title..." />
+        
+        <input class="submit" type="submit" name="find_title"
+               value="Search"/>
+    
+    </form>
+    
+    <!-- end of Title Search -->
+    
+    <hr />
+    <!-- Start of Author Search -->
+    Author Search <br />
+    <!-- end of Author Search -->
+    
+    <!-- Start of Genre Search -->
+    Genre Search <br />
+    <!-- End of Genre Search-->
+    
+    <!-- Start of Rating Search -->
+    Rating Search
+    <!-- End of rating Search -->
+    
+
+<div class="box footer">
+    CC Troy Kennedy 2020
+</div>    <!-- / footer -->
+    
+</div>  <!-- / wrapper  -->    
+
+</body>    
